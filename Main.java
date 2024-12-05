4b407046aa8ffe3c41a1c4d704b7114146eaf211
Patryk Wróblewski <102994893+Patryk2001k@users.noreply.github.com>
2024-12-06 00:04:11 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -8,17 +8,27 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
+
+        /**/
         Engine myEngine = new Engine();
         myEngine.start();
         Connection newConnection = myEngine.returnConnection();
         UsersDAO newUsersDAO = new UsersDAO(newConnection);
         List<HashMap<String, String>> testList = new ArrayList<>();
-        testList = newUsersDAO.createUser("testUser1", "password1", "doctor", "testName", "TestSurname");
+        //testList = newUsersDAO.createUser("testUser1", "password1", "doctor", "testName", "TestSurname");
+        //System.out.println(testList);
+        //boolean testBoolean = newUsersDAO.isUserValid("testUser1", "password1");
+        //System.out.println(testBoolean);
+        //testList = newUsersDAO.getUser("testUser123", "password1");
+        //System.out.println(testList);
+        //testList = newUsersDAO.isUserValid("testUser11", "password1");
+        //System.out.println(testList);
+        //testList = newUsersDAO.deleteUser("testUser12");
+        //System.out.println(testList);
+        System.out.println("Testowy");
+        testList = newUsersDAO.updateUserPassword("testUser1", "newPassword");
         System.out.println(testList);
-        boolean testBoolean = newUsersDAO.isUserValid("testUser1", "password1");
-        System.out.println(testBoolean);
-        System.out.println(newUsersDAO.getUser("testUser1", "password1"));
-
-
+        testList = newUsersDAO.updateUserPassword("testUser11", "newPassword");
+        System.out.println(testList);
     }
 }
